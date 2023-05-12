eb5e46283c3d49e90628f0e35e7d050c4cc1dcd0
Shefali Patel <shefali.p889@gmail.com>
2023-05-12 13:55:55 +0530

    first Commit

---
+++ src/component/Buttonlist.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Buttonlist = () => {
+  return (
+    <div className='flex m-3 '>
+    <button className='p-3 shadow-sm border border-grey-100 rounded-xl m-3'>Watch</button>
+    <button className='p-3 shadow-sm border border-grey-100 rounded-xl m-3'>Drama</button>
+    <button className='p-3 shadow-sm border border-grey-100 rounded-xl m-3'>Movies</button>
+    <button className='p-3 shadow-sm border border-grey-100 rounded-xl m-3'>Songs</button>
+    <button className='p-3 shadow-sm border border-grey-100 rounded-xl m-3'>Live</button>
+    <button className='p-3 shadow-sm border border-grey-100 rounded-xl m-3'>Trending</button>
+    <button className='p-3 shadow-sm border border-grey-100 rounded-xl m-3'>News</button>
+    </div>
+  )
+}
+
+export default Buttonlist
